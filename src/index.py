f5de76ae635871bbe01f95e588e1dc244786576a
hureta <40241920+0t3k25@users.noreply.github.com>
2021-11-02 03:15:50 +0000

    add config

--- src/index.py
+++ src/index.py
@@ -3,14 +3,14 @@ from pathlib import Path
 import cv2
 import numpy as np
 
-# 受け取ったファイルの形式が正しいか確認
-# 画像読み込み
+# 受け取ったファイルの形式が正しいか確認（あとで書く）
+
+# 画像までのパス
 image_path = Path(__file__).parent / "sample.jpeg"
+# 画像読み込み
 image = cv2.imread(image_path.as_posix())
-# 画像の大きさを1280×720の大きさに変更
 print(image.shape)
-# 画像の大きさ変更
-image_resized = cv2.resize(image, dsize=(1280, 720))
+"""
 # processing
 xs, xt = 0, 144
 ys, yt = 0, 82
@@ -24,17 +24,23 @@ cv2.imwrite((image_path.parent / "output.jpeg").as_posix(), trimmed_image)
 # print(height)
 # print(width)
 print(image_resized.shape)
+"""
+# キーキャップの縦横比率
+keycap_height: "int" = 18
+keycap_width: "int" = 18
+height_width_ratio = keycap_width / keycap_height
+# キーの横の長さ
+total_height = 1.8 * 5
+total_width = 1.8 * 13 + 3.8
+# 最低限の画像の大きさを計算
 
-# キーキャップの比率
-keycap_height: "int" = 15
-keycap_width: "int" = 13
-ratio = keycap_width / keycap_height
-print(ratio)
 # キーの作成
 # 座標を切る
 # 最初の場所を決めて切る、最初の場所がわかれば次の場所もわかるのでそこも同じように切る。
 # とりあえず同じ大きさで切っていき、一番小さいキーを作る。
 # keycap 初期化
+keycap_line_1 = [[[y, dy], [x, dx]]]
+"""
 keycap_line_1 = [[[0, 1], [82 * i, 82 * (i + 1)]] for i in range(13)]  # リスト内包表記
 keycap_line_1.append([[0, 144], [82 * 13, 1280]])  # backspace
 
@@ -60,10 +66,6 @@ for keycap_line in keycap:
 
 for i, image in enumerate(trimmed_image_list):
     cv2.imwrite((image_path.parent / f"output_{i:04}.jpeg").as_posix(), image)
+print(keycap)
 
-
-def a(c: int) -> int:
-    if c == 1:
-        return 1
-    else:
-        return 2
+"""
