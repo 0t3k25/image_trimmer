adb8cf76948136e64c559d517ae64bb11534b333
hureta <40241920+0t3k25@users.noreply.github.com>
2021-11-16 16:34:17 +0000

    create join_picture_outputclass

---
+++ src/outputmodule/createpdf.py
@@ -0,0 +1,9 @@
+class OutputData:
+    def __init__(self,keycap_pictures,blank_picture):
+        self.keycap_pictures = keycap_pictures
+        self.blank_picture = blank_picture
+    #写真結合
+    def join_picture(self):
+
+    #結合した写真をpdfとして出力
+    def create_pdf(self):
