86bf82ff47991acba7cb7eaf3ab122bcb5ade443
hureta <40241920+0t3k25@users.noreply.github.com>
2021-11-18 00:00:46 +0900

    join pictures and create pdf class

--- src/outputmodule/createpdf.py
+++ src/outputmodule/createpdf.py
@@ -1,9 +1,9 @@
 class OutputData:
-    def __init__(self,keycap_pictures,blank_picture):
-        self.keycap_pictures = keycap_pictures
+    def __init__(self,keycap_picture,blank_picture):
+        self.keycap_picture = keycap_picture
         self.blank_picture = blank_picture
     #写真結合
     def join_picture(self):
-
+        self.keycap_picture = 
     #結合した写真をpdfとして出力
     def create_pdf(self):
