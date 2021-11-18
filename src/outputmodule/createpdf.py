b44ab3a690d09e91f08e2a69edbe05b9c47c426b
hureta <40241920+0t3k25@users.noreply.github.com>
2021-11-18 17:37:59 +0000

    add little code about join_picture

--- src/outputmodule/createpdf.py
+++ src/outputmodule/createpdf.py
@@ -1,9 +1,19 @@
 class OutputData:
-    def __init__(self,keycap_picture,blank_picture):
-        self.keycap_picture = keycap_picture
+    def __init__(self, keycap_lines, blank_picture):
+        self.keycap_lines = keycap_lines
         self.blank_picture = blank_picture
-    #写真結合
+
+    # トリミング写真結合
     def join_picture(self):
-        self.keycap_picture = 
-    #結合した写真をpdfとして出力
+        for keycap_line in self.keycap_lines:
+            trimmed_image_list = []
+            image_height = keycap_line[0][0][1]  # その列の画像の高さ
+            for keycap in keycap_line:
+                trimmed_image_list.append(keycap)
+                if !=lastnum:
+                    append
+                else:
+                    opencv結合
+    # 結合した写真をpdfとして出力
     def create_pdf(self):
+        self.keycap_pictures
